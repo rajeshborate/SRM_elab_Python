08626467c3d1bddf7045178ab9c5c1f690040d5b
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-01 18:31:32 +0530

    Add files via upload

---
+++ level_01_session_01_1811110024_Day Old Bread.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 18:19:42 2019
+
+@author: Rajesh D Borate
+"""
+
+n=int(input())
+
+reg_price=n*185
+dis=0.6*reg_price
+s=reg_price-dis
+print("Loaves Discount")
+print("Regular Price",reg_price)
+print("Total Discount",dis)
+print("Total Amount to be paid",s)

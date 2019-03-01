603884a72eac7aa2639ca06bcccf6398bab93184
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-01 18:25:49 +0530

    Add files via upload

---
+++ level_01_session_01_1811110003_Finance Sector.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 18:19:42 2019
+
+@author: Rajesh D Borate
+"""
+
+p=int(input())
+r=int(input())
+t=int(input())
+
+i=p*r*t/100
+a=i+p
+print("Interest=",i)
+print("Amount=",a)

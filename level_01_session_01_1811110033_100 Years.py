17bd0044535a44113228e90151e75d74e4acbb25
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-01 18:31:50 +0530

    Add files via upload

---
+++ level_01_session_01_1811110033_100 Years.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 18:19:42 2019
+
+@author: Rajesh D Borate
+"""
+
+s=input()
+age=int(input())
+a=100-age+2017
+print("{} will be 100 years old in {}".format(s,a))

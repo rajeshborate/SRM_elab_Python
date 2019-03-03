2faa9d0f900b469c7648d55844ddf957fc7c2ef5
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-03 06:30:32 +0530

    Add files via upload

---
+++ level_01_session_01_1811110028_Display the user Address.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 06:29:29 2019
+
+@author: Rajesh D Borate
+"""
+
+n=int(input())
+s=input()
+p=int(input())
+print("Address")
+print("House Number:",n)
+print("Area:",s)
+print("Pin Code:",p)

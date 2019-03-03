603ee387074b43b4607cc5a665e657a0815c54a0
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-03 06:29:11 +0530

    Add files via upload

---
+++ level_01_session_01_1811110012_eLab Python Calculator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 06:26:49 2019
+
+@author: Rajesh D Borate
+"""
+
+import math
+a=int(input())
+b=int(input())
+add=a+b
+sub=a-b
+pro=a*b
+div=a/b
+rem=a%b
+exp=a**b
+l=math.log(a)/math.log(10)
+print("Addition=",add)
+print("Subtraction=",sub)
+print("Product=",pro)
+print("Division=",div)
+print("Remainder=",rem)
+print("Log=",l)
+print("Exponent=",exp)

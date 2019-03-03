6428348279cccbe0ac60ce5247e9fad51e49c32a
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-03 06:31:57 +0530

    Add files via upload

---
+++ level_01_session_01_1811110006_Area of a Room.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 06:30:49 2019
+
+@author: Rajesh D Borate
+"""
+
+a=float(input())
+b=float(input())
+area= a*b
+print("The area of the room is {0:.2f} square feet".format(area))

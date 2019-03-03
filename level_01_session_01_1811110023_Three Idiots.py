58497f72cb1f967fd6e9d36e5b2e31cbd8c02154
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-03 06:34:24 +0530

    Add files via upload

---
+++ level_01_session_01_1811110023_Three Idiots.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 06:33:27 2019
+
+@author: Rajesh D Borate
+"""
+
+a=int(input())
+b=int(input())
+c=int(input())
+l=[a,b,c]
+l.sort()
+print("The minimum value is",l[0])
+print("The maximum value is",l[2])
+print("The middle value is",l[1])
+    

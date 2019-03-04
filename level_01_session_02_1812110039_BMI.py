4009e9562f3d0f6d1beecbf142bc43bb671562cc
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-04 05:17:30 -0800

    Add files via upload

---
+++ level_01_session_02_1812110039_BMI.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 18:46:12 2019
+
+@author: Rajesh D Borate
+"""
+
+a=int(input())
+b=int(input())
+if(a<45 and b<22):
+    print("Low")
+elif(a>45 and b<22):
+    print("Medium")
+else:
+    print("High")

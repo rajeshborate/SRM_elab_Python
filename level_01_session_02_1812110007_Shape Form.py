dce5207ac5695e6d969f964795479cb8c84afe34
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-04 05:18:54 -0800

    Add files via upload

---
+++ level_01_session_02_1812110007_Shape Form.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 18:47:37 2019
+
+@author: Rajesh D Borate
+"""
+
+n=int(input())
+if(n==9):
+    print("Nanogon")
+elif(n==10):
+    print("Decagon")
+elif(n==8):
+    print("Octagon")
+elif(n==7):
+    print("Heptagon")
+else:
+    print("Input should be from 7 to 10")

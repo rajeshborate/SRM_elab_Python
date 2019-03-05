5fe0b56806920949283e6caabc596c9db2d4de55
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-05 06:26:27 -0800

    Add files via upload

---
+++ level_01_session_03_1813110042_FACTORS.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 19:55:36 2019
+
+@author: Rajesh D Borate
+"""
+n=int(input())
+
+for i in range(1,n+1):
+    if(n%i==0):
+        print(i)
+    

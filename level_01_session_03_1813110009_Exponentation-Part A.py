eb6096b5f81b80d94cc67a0de171d63105670fd2
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-12 14:42:17 +0530

    Add files via upload

---
+++ level_01_session_03_1813110009_Exponentation-Part A.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 14:38:34 2019
+
+@author: Rajesh D Borate
+"""
+
+lower = 100
+upper = 2000
+
+# To take input from the user
+# lower = int(input("Enter lower range: "))
+# upper = int(input("Enter upper range: "))
+
+for num in range(lower, upper + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   # find the sum of the cube of each digit
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)

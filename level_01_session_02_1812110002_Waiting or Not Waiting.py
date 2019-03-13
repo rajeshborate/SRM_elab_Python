37d81bb76c5e4e1e253947480b788b8f3c276f0c
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-03-13 17:54:56 +0530

    Add files via upload

---
+++ level_01_session_02_1812110002_Waiting or Not Waiting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 17:53:48 2019
+
+@author: Rajesh D Borate
+"""
+
+n=int(input())
+if(n>0):
+    print("I am waiting")
+else:
+    print("I am not waiting")
